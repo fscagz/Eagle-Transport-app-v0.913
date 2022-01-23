4451dce6fb09fcca6334e560a9b69af0ea950d5a
F. Scagz <enjoysomecheese@gmail.com>
2022-01-22 20:50:58 -0500

    Initial Commit

---
+++ Eagle Transport/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Eagle Transport
+//
+//  Created by Frank Scagluso Jr. on 1/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Eagle Transport/Eagle_TransportApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eagle_TransportApp.swift
+//  Eagle Transport
+//
+//  Created by Frank Scagluso Jr. on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Eagle_TransportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
